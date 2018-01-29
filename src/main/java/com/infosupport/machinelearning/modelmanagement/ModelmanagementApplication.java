2621b41a0c7eed3f242a6bf7274e947c83c08ba4
Willem <willemm@infosupport.com>
2018-01-29 09:11:06 +0100

    Add project files

---
+++ src/main/java/com/infosupport/machinelearning/modelmanagement/ModelmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.infosupport.machinelearning.modelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelmanagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infosupport/machinelearning/modelmanagement/ModelmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosupport.machinelearning.modelmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelmanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
