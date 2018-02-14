3f8c79e91489c723b57b5c5c2e85688818c76175
ChristelG <christel.geurts@infosupport.com>
2018-02-14 14:22:25 +0100

    moved some of the file creation code from ModelsController to ModelFileService.
    updated .gitignore.

--- src/main/java/com/infosupport/machinelearning/modelmanagement/services/ModelFileService.java
+++ src/main/java/com/infosupport/machinelearning/modelmanagement/services/ModelFileService.java
@@ -1,4 +1,34 @@
 package com.infosupport.machinelearning.modelmanagement.services;
 
+import com.infosupport.machinelearning.modelmanagement.models.Model;
+import com.infosupport.machinelearning.modelmanagement.repositories.ModelRepository;
+
+import java.io.File;
+
 public class ModelFileService {
+
+    public int getCurrentModelVersion(ModelRepository modelRepository, String name) {
+        // Gets latest version from model repository and adds 1.
+        Model currentModel = modelRepository.findTopByNameOrderByVersionDesc(name);
+        int newVersionNumber = 1;
+        if (currentModel != null) {
+            newVersionNumber = currentModel.getVersion() + 1;
+        }
+        return newVersionNumber;
+    }
+
+    public String createDirectories(String modelRootDirectoryPath, String name) {
+        // Create root directory
+        File modelRootDirectory = new File(modelRootDirectoryPath);
+        if (!modelRootDirectory.exists()) {
+            modelRootDirectory.mkdirs();
+        }
+        // Create folder for model
+        String modelDirPath = modelRootDirectoryPath + File.separator + name;
+        File modelDirectory = new File(modelDirPath);
+        if (!modelDirectory.exists()) {
+            modelDirectory.mkdir();
+        }
+        return modelDirPath;
+    }
 }
