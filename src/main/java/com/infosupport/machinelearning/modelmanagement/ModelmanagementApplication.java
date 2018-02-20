bf2a3cf743596f40f8bb91d96c73a09b22089cb6
Willem Meints <willemm@infosupport.com>
2018-02-20 15:53:35 +0100

    Remove commented code

--- src/main/java/com/infosupport/machinelearning/modelmanagement/ModelmanagementApplication.java
+++ src/main/java/com/infosupport/machinelearning/modelmanagement/ModelmanagementApplication.java
@@ -5,18 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(StorageProperties.class)
 public class ModelmanagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ModelmanagementApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner init(StorageService storageService) {
-//        return (args) -> {
-//            //storageService.deleteAll();
-//            storageService.init();
-//        };
-//    }
 }
